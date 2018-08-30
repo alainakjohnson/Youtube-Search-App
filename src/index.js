23fa06682995a75c9f7782ecef5ca2e99c7e542a
alainakjohnson <akjohnson@valdosta.edu>
2018-08-30 14:04:54 +0000

    completed step 1

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const API_KEY = 'AIzaSyBqiPN9f0LoYUfEFf8DxwMhb2sF-ad_3-I';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
