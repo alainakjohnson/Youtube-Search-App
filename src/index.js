7df6217107c0ee09e453f804deb8da792be277c1
alainakjohnson <akjohnson@valdosta.edu>
2018-08-30 14:27:22 +0000

    completed search bar 2.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SearchBar from "./components/search_bar";
 import registerServiceWorker from './registerServiceWorker';
 
 const API_KEY = 'AIzaSyBqiPN9f0LoYUfEFf8DxwMhb2sF-ad_3-I';
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
