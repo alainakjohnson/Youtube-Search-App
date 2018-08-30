d2fa5789655bbae27bc2e9cc8338f36491952264
alainakjohnson <akjohnson@valdosta.edu>
2018-08-30 15:15:00 +0000

    completed 2.3

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 
 class SearchBar extends React.Component {
     render() {
-        return <input onChange={ (event) => console.log(event.target.value) }/>
+        return <input onChange={ (event) => this.setState( { term: event.target.value}) }/>
     }
+    
+        constructor(props) {
+        super(props);
+
+        this.state = { term: "" };
+    }
+    
 }
 
 export default SearchBar;
