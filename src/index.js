8fd1541d4d135484ce7f3242af10dfdc6547c1b1
alainakjohnson <akjohnson@valdosta.edu>
2018-09-04 15:40:46 +0000

    fixed videos, complete and working up through 2.5

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import SearchBar from "./components/search_bar";
 import YTSearch from 'youtube-api-search';
 import registerServiceWorker from './registerServiceWorker';
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
 const API_KEY = 'AIzaSyBqiPN9f0LoYUfEFf8DxwMhb2sF-ad_3-I';
 
@@ -18,11 +19,13 @@ class App extends Component {
             // ES6 syntax for this.setState({ videos: videos});
             this.setState({ videos });
         });
+        
     }
     render() {
         return (
             <div>
                 <SearchBar />
+                 <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
