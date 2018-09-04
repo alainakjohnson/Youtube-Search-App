fd4c512a734ccb09ad9ce5be0b101e0b7ea56aba
alainakjohnson <akjohnson@valdosta.edu>
2018-09-04 14:34:09 +0000

    finished youtube 1

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import SearchBar from "./components/search_bar";
+import YTSearch from 'youtube-api-search';
 import registerServiceWorker from './registerServiceWorker';
 
 const API_KEY = 'AIzaSyBqiPN9f0LoYUfEFf8DxwMhb2sF-ad_3-I';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends Component {
+     constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+            // ES6 syntax for this.setState({ videos: videos});
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
