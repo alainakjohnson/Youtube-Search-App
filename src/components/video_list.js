567423fab81faa4aa53a9bcb6a932b01bd91cbea
alainakjohnson <akjohnson@valdosta.edu>
2018-09-05 06:59:00 +0000

    completed youtube search 2 - fixed layout

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,10 +4,9 @@ import VideoListItem from "./video_list_item";
 const VideoList = props => {
     const videoIterms = props.videos.map(video => {
         return <VideoListItem key={video.etag} video={video} />;
-        return <ul className="col-md-4 list-group">{videoIterms}</ul>;
     });
 
-    return <ul>{ videoIterms }</ul>;
+    return <ul className="col-md-4 list-group">{videoIterms}</ul>;
 };
 
 export default VideoList;
